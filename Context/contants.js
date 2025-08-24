1d61f399ff3925de009b7717fc83333be5541b20
TeslaInch <nikolaincarnate@gmail.com>
2025-08-23 22:05:13 -0700

    final push

--- Context/contants.js
+++ Context/contants.js
@@ -2,5 +2,5 @@ import crowdFunding from "./CrowdFunding.json";
 
 
 //crowdFunding marketplace
-export const CrowdFundingAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+export const CrowdFundingAddress = '0x6B2fC64029821dF463b151aD674e33e3d6e7EC18';
 export const CrowdFundingABI = crowdFunding.abi;
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config(); // Load environment variables from .env
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    // Ethereum Sepolia testnet
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/aDuEJGbVAA-ZzbjDYGS-S",
+      accounts: [process.env.PRIVATE_KEY]
+    },
+
+    // Base Sepolia testnet
+    baseSepolia: {
+      url: "https://sepolia.base.org",
+      accounts: [process.env.PRIVATE_KEY]
+    },
+
+    // Base mainnet
+    base: {
+      url: "https://mainnet.base.org",
+      accounts: [process.env.PRIVATE_KEY]
+    },
+
+    // Ethereum mainnet (when you're ready to go live)
+    mainnet: {
+      url: "https://rpc.ankr.com/eth",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };

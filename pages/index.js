e52d45a0f0fc0efa104e46b1c4d516dce917d8c6
TeslaInch <nikolaincarnate@gmail.com>
2025-08-24 00:08:15 -0700

    finally it is doen ejeh mi

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import { useEffect, useState, useContext } from "react";
 // Internal imports
 import { CrowdFundingContext } from "../Context/CrowdFunding";
 // import { Hero, Card, PopUp } from "../Components";
-import Hero from "../components/Hero"
-import Card from "../components/Card"
-import PopUp from "../components/PopUp"
+import Hero from "../Components/Hero"
+import Card from "../Cardomponents/Card"
+import PopUp from "../Components/PopUp"
 
 const Index = () => {
   const {
